0ee9cbdb8f0607fffa942e8a634e84609951261a
DavideMaffi <davide1.maffi@mail.polimi.it>
2023-05-09 16:26:39 +0200

    Improved Tailwind fast refresh

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <!-- Home page -->
-
 <template>
     <div class="flex flex-col items-center justify-evenly h-full">
-        <img src="~/assets/i3lab.png" alt="i3lab"/>
-        <h1 class="text-6xl border-black">
+        <img src="../assets/i3lab.png" alt="i3lab"/>
+        <h1 class="text-6xl">
             First Draft
         </h1>
     </div>

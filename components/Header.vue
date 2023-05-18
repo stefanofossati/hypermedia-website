475d1f7dac533ddc1649142738bc97cb64682bee
Davidedm99 <51082176+Davidedm99@users.noreply.github.com>
2023-05-18 16:25:50 +0200

    header Draft

--- components/Header.vue
+++ components/Header.vue
@@ -1,47 +1,34 @@
 <!-- Header of the page -->
 
 <template>
-    <header class="fixed top-0 w-screen bg-zinc-200">
-        <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-            <div class="flex lg:flex-1">
-                <NuxtLink to='/' class="-m-1.5 p-1.5">
-                    <span class="sr-only">StartMeUp</span>
-                    <img class="h-20 w-auto" src="../assets/LogoDraft.svg" alt="StartMeUp logo"/>
-                </NuxtLink>
+    <header class="fixed top-0 w-screen">
+        <nav class="bg-gray-800">
+            <div class="max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div class="relative flex h-28 ">
+                    <div class="flex-1 sm:items-stretch sm:justify-start">
+                        <div class="flex flex-shrink-0 items-center -mt-4">
+                            <img class="block h-36 ml-32 w-auto mr-auto cursor-pointer" src="../assets/LogoDraft.svg" alt="StartMeUp">
+                            <div class="flex space-x-12 sm:ml-6 sm:block -mr-32">
+                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">Areas</NuxtLink>
+                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">Projects</NuxtLink>
+                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">...</NuxtLink>
+                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">About Us</NuxtLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="flex lg:hidden">
-                <button type="button" class="-m-2.5 inline-flex item-center justify-center rounded-md p-2.5 text-stone-900"
-                        @click="menuOpen = true">
-                    <span class="sr-only">Main Menu</span>
-                </button>
-            </div>
-
-            <PopoverGroup class="hidden lg:flex lg:gap-x-15">
-                <Popover class="relative">
-                    <PopoverButton class="flex items-center gap-x-1 text-sm font-bold leading-5 text-stone-900">
-                        <NuxtLink>Areas</NuxtLink>
-                        <img src="../assets/chevron-down-solid.svg" class="h-5 w-5 flex-none" aria-hidden="true"/>
-                    </PopoverButton>
-                </Popover>
-            </PopoverGroup>
-
-
         </nav>
     </header>
 </template>
 <style>
+    *{
+        font-family: 'Roboto', sans-serif;
+    }
 /*.router-link-exact-active {*/
 /*    color: red;*/
 /*}*/
 </style>
+
 <script setup lang="ts">
-import {
-    Popover,
-    PopoverButton,
-    PopoverGroup,
-} from '@headlessui/vue'
-
-import {ref} from 'vue'
-const menuOpen = ref(false)
-
 </script>

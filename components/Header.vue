ac384abc082a97e724ee5dcb7c7f589082895ab2
Davidedm99 <51082176+Davidedm99@users.noreply.github.com>
2023-05-19 14:46:12 +0200

    henlo

--- components/Header.vue
+++ components/Header.vue
@@ -9,9 +9,9 @@
                         <div class="flex flex-shrink-0 items-center -mt-4">
                             <img class="block h-36 ml-32 w-auto mr-auto cursor-pointer" src="../assets/LogoDraft.svg" alt="StartMeUp">
                             <div class="flex space-x-12 sm:ml-6 sm:block -mr-32">
-                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">Areas</NuxtLink>
-                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">Projects</NuxtLink>
-                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">...</NuxtLink>
+                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">All Areas</NuxtLink>
+                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">All Projects</NuxtLink>
+                                <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">Team</NuxtLink>
                                 <NuxtLink to="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold cursor-pointer">About Us</NuxtLink>
                             </div>
                         </div>

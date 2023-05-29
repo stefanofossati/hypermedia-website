61bec841dc5f2a4773f57168cd3de32a22b1df41
DavideMaffi <davide1.maffi@mail.polimi.it>
2023-05-29 17:21:58 +0200

    the team page is now responsive

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -15,14 +15,14 @@ const {data: team, error}: { data: Person[] } = await useFetch(
 <template>
   <div> <!-- page with team's profile pictures and names -->
     <div class="flex flex-col items-center text-center">
-      <span class="text-6xl font-bold m-10 dark:text-white">Team</span>
-      <div class="grid grid-cols-4 gap-5 w-full px-5">
+      <span class="text-3xl sm:text-4xl md:text-6xl font-bold m-10 dark:text-white">Team</span>
+      <div class="flex flex-col items-center md:grid md:place-items-center md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 w-full px-5">
         <div v-for="person in team">
           <NuxtLink :to="`/team/${person.name}_${person.surname}`"
-                    class="hover:animate-pulse flex flex-col items-center rounded-xl bg-gray-600 p-1.5 mb-10">
-            <span class="text-6xl text-gray-300 my-3">{{ person.name }}</span>
-            <span class="text-6xl text-gray-300 my-3">{{ person.surname }}</span>
-            <img :src="person.img" :alt="`${person.name} ${person.surname}'s profile pic`" class="rounded-b-xl h-96"/>
+                    class="flex flex-col items-center rounded-xl bg-gray-600 p-1.5 mb-10 w-fit text-xl sm:text-2xl md:text-3xl text-gray-300">
+            <span class="my-3">{{ person.name }}</span>
+            <span class="my-3">{{ person.surname }}</span>
+            <img :src="person.img" :alt="`${person.name} ${person.surname}'s profile pic`" class="rounded-b-xl h-64 md:h-72 lg:h-96 w-full"/>
           </NuxtLink>
         </div>
       </div>

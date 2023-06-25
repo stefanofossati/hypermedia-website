04b982e4a3beb8b80ccef85e1ce7d270cb15bc61
stefanofossati <10569836@mail.polimi.it>
2023-06-25 18:45:03 +0200

    change big image position

--- pages/projects/[project].vue
+++ pages/projects/[project].vue
@@ -12,7 +12,7 @@
     <div class="sm:grid sm:grid-cols-4 sm:gap-3 py-3 px-5 block">
 
       <!-- Project Info Mobile -->
-      <div class="sm:hidden flex-col py-1 px-1 bg-yellow-800 relative">
+      <div class="sm:hidden flex-col py-1 px-1 relative">
         <button @click="hide_function" class="absolute top-0 right-0">
           <svg id="button_info" viewBox="0 -960 960 960" class="dark:text-white text-black fill-current h-16 w-16">
             <path
@@ -35,7 +35,7 @@
 
       <!-- Project Info Desktop -->
       <div
-          class="sm:col-span-1 sm:sticky sm:inset-y-0 sm:right-0 sm:flex sm:flex-col border-l-2 border-slate-700 dark:border-gray-500 hidden">
+          class="sm:col-span-1 sm:sticky sm:inset-y-0 sm:right-0 sm:flex sm:flex-col border-l-2 border-slate-700 dark:border-gray-500 hidden self-center">
         <ProjectsProjectInfo :info_supervisor="pj.supervisor" :info_budget="pj.budget" :info_start_date="pj.start_date"
                              :info_end_date="pj.end_date"/>
       </div>
@@ -44,35 +44,43 @@
     <!-- Images -->
     <div class="py-4">
       <div class="grid grid-cols-12 grid-rows-4 gap 3 justify-items-center place-items-center">
-        <div class="col-span-1 row-span-1"></div>
-        <div class="col-span-4 row-span-4 self-center">
-          <img id="big_image" :src="pj.gallery_images[0].url" alt="" class=" h-96 w-full object-cover">
-        </div>
-        <div class="col-span-7 row-span-1 text-black dark:text-white text-3xl">
+        <div class="col-span-7 row-span-1 text-black dark:text-white text-3xl justify-self-start px-3">
           Project Gallery
         </div>
+
+        <div class="col-span-4 row-span-4 self-center">
+          <img id="big_image" :src="pj.gallery_images[3].url" alt="" class=" h-96 w-full object-cover">
+        </div>
+        <div class="col-span-1 row-span-1"></div>
         <div v-on:click="go_left(pj.gallery_images.length, pj.gallery_images)" class="col-span-1 row-span-2 hover:animate-pulse">
           <svg viewBox="0 -960 960 960" class="dark:text-white text-black fill-current h-16 w-16 mr-1.5 mb-1.5 justify-center">
             <path d="M400-80 0-480l400-400 56 57-343 343 343 343-56 57Z"/>
           </svg>
         </div>
         <div  class="col-span-2 row-span-2 bg-gray-100 " >
-          <img id="image01" :src="pj.gallery_images[1].url" alt="" class=" h-48 w-full object-cover " >
+          <img id="image01" :src="pj.gallery_images[0].url" alt="" class=" h-48 w-full object-cover " >
         </div>
         <div  class="col-span-2 row-span-2 " >
-          <img id="image02" :src="pj.gallery_images[2].url" alt=""  class=" h-48 w-full object-cover ">
+          <img id="image02" :src="pj.gallery_images[1].url" alt=""  class=" h-48 w-full object-cover ">
         </div>
         <div  class="col-span-2 row-span-2 bg-gray-100 ">
-          <img id="image03" :src="pj.gallery_images[3].url" alt="" class=" h-48 w-full object-cover ">
+          <img id="image03" :src="pj.gallery_images[2].url" alt="" class=" h-48 w-full object-cover ">
         </div>
         <div v-on:click="go_right(pj.gallery_images.length, pj.gallery_images)" class="col-span-1 row-span-2 hover:animate-pulse">
           <svg viewBox="0 -960 960 960" class="dark:text-white text-black fill-current h-16 w-16 mr-1.5 mb-1.5 justify-center ">
             <path d="m304-82-56-57 343-343-343-343 56-57 400 400L304-82Z"/>
           </svg>
         </div>
+        <div  class="col-span-7 row-span-1 justify-end text-black dark:text-white flex flex-row h-fit w-full border-t-2 border-slate-500">
+          <p id="description_big_image" class="self-center ">
+            {{ pj.gallery_images[(i+3) % (pj.gallery_images.length)].description }}
+          </p>
+          <svg viewBox="0 -960 960 960" class="dark:text-white text-black fill-current h-16 w-16 mr-1.5 mb-1.5 self-center">
+            <path d="M560-280 360-480l200-200v400Z"/>
+          </svg>
+
         <div class="col-span-1 row-span-1"></div>
-        <div id="description_big_image" class="col-span-7 row-span-1 justify-self-start text-black dark:text-white">
-          {{ pj.gallery_images[i].description }}
+
         </div>
 
       </div>
@@ -119,17 +127,18 @@ function go_left(length: number, images_src: ImageGallery[]) {
   const description_big_image: HTMLElement | null = document.getElementById("description_big_image")!;
 
   i = (i+1) % (length);
-  big_image.src = images_src[i].url;
-  description_big_image.innerHTML = images_src[i].description;
+  image_01.src = images_src[i].url;
+
 
   let j = (i+1) % (length);
-  image_01.src = images_src[j].url;
+  image_02.src = images_src[j].url;
 
   let k = (i+2) % (length);
-  image_02.src = images_src[k].url;
+  image_03.src = images_src[k].url;
 
   let l = (i+3) % (length);
-  image_03.src = images_src[l].url;
+  big_image.src = images_src[l].url;
+  description_big_image.innerHTML = images_src[l].description;
 
 }
 
@@ -141,17 +150,20 @@ function go_right(length:number, images_src:ImageGallery[]){
   const description_big_image: HTMLElement | null = document.getElementById("description_big_image")!;
 
   i = (i-1+length) % (length);
-  big_image.src = images_src[i].url;
-  description_big_image.innerHTML = images_src[i].description;
+  image_01.src = images_src[i].url;
 
   let j = (i+1) % (length);
-  image_01.src = images_src[j].url;
+
+  image_02.src = images_src[j].url;
 
   let k = (i+2) % (length);
-  image_02.src = images_src[k].url;
+  image_03.src = images_src[k].url;
+
 
   let l = (i+3) % (length);
-  image_03.src = images_src[l].url;
+
+  big_image.src = images_src[l].url;
+  description_big_image.innerHTML = images_src[l].description;
 }
 
 </script>

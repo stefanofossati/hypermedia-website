dd0ddc6de0fe0432cee0f8c8c781900cd7d25d35
Davidedm99 <51082176+Davidedm99@users.noreply.github.com>
2023-06-28 12:05:03 +0200

    working on project carousel

---
+++ components/Areas/AreaCarousel.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+const props: Readonly<{
+  area_title?: string,
+  area_image?: string[],
+  id?: string,
+  area_color?: string,
+  area_description?: string;
+}> = defineProps(['area_title', 'area_image', 'area_color', 'area_description', 'id']);
+
+</script>
+
+<template>
+  <div>
+    <!-- Carousel wrapper -->
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+      <!-- Items -->
+      <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+        <img :src="props.area_image" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>

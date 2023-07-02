a9fc4f431298e90c209215dcede538b071a4ec9a
Davidedm99 <51082176+Davidedm99@users.noreply.github.com>
2023-07-02 20:19:03 +0200

    fixed automatic carousel working on the "button" one

--- pages/areas/[area].vue
+++ pages/areas/[area].vue
@@ -3,20 +3,16 @@
     <!--Automatic Carousel of images-->
     <div id="carousel" class="relative w-full">
       <!-- Carousel -->
-      <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+      <div class="relative h-52 overflow-hidden md:h-96">
         <!-- Items -->
-        <div class="transition-opacity duration-1000 ease-in-out" v-for="area in areas.area_image">
-          <img :src="area"
+        <div class="transition-all delay-150 duration-2000 ease-in-out" id="0">
+          <img :src="areas.area_image[0]"
                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
-<!--        <div class="hidden transition-opacity duration-1000 ease-in-out" id="2">-->
-<!--          <img src="https://media.istockphoto.com/id/1093110112/photo/picturesque-morning-in-plitvice-national-park-colorful-spring-scene-of-green-forest-with-pure.jpg?s=612x612&w=0&k=20&c=lpQ1sQI49bYbTp9WQ_EfVltAqSP1DXg0Ia7APTjjxz4="-->
-<!--               class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">-->
-<!--        </div>-->
-<!--        <div class="hidden transition-opacity duration-1000 ease-in-out" id="3">-->
-<!--          <img src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="-->
-<!--               class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">-->
-<!--        </div>-->
+        <div class="hidden transition-all delay-150 duration-2000 ease-in-out" id="1">
+          <img :src="areas.area_image[1]"
+               class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        </div>
       </div>
     </div>
 
@@ -42,13 +38,13 @@
                           :main_image="pj.main_image"/>
 
           <!-- Buttons -->
-          <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+          <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onclick="carouselPrev" >
             <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-700/80 group-hover:bg-white/50 dark:group-hover:bg-white/80 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
                 <span class="sr-only">Previous</span>
             </span>
           </button>
-          <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+          <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onclick="carouselNext" >
             <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-700/80 group-hover:bg-white/50 dark:group-hover:bg-white/80 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
                 <span class="sr-only">Next</span>
@@ -74,31 +70,30 @@ const {data: areas, error}: { data: Area } = await useFetch(
     }
 );
 
-let cycleValue = 1;
-let interval: any;
-window.onload=timer;
+//automatic carousel change, need two timeout to infer the changes, still problems in the transition not smooth
+let currentImage = 0;
+const seconds = 5;
 
-function timer() {
-  interval = setTimeout(changeImage, 3000);
-}
-
-function resetTimer() {
-  clearTimeout(interval);
-  interval = setTimeout(changeImage, 3000);
-}
-
+const timeoutId = setTimeout(() =>{
+  changeImage();
+}, seconds * 1000);
 function changeImage(){
-  document.getElementById(cycleValue.toString())?.classList.add("hidden");
-  document.getElementById(((cycleValue+1==4)?cycleValue=1:cycleValue+1).toString())?.classList.remove("hidden");
+  document.getElementById((currentImage).toString())?.classList.add("hidden");
+  document.getElementById((currentImage == 1 ? currentImage = 0 : currentImage = currentImage + 1).toString())?.classList.remove("hidden");
 
-  if(cycleValue < 4 ){
-    cycleValue += cycleValue;
-  }else{
-    cycleValue = 1;
-  }
-
-  resetTimer();
+  const imageTimeout = setTimeout(() =>{
+    changeImage();
+  }, seconds * 1000);
 }
+
+function carouselNext(){
+
+}
+
+function carouselPrev(){
+
+}
+
 
 </script>
 
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -34,13 +34,15 @@
     </div>
 
     <div id="all_projects" class="px-10 hidden">
-      <div class="grid xl:grid-cols-4 gap-4 lg:grid-cols-3 md:grid-cols-2 col-span-1 place-content-center px-2 py-2">
+      <div class="grid xl:grid-cols-4 gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1
+                  place-content-center px-2 py-2">
         <ProjectPreview v-for="pj in projects"
                         :project_title="pj.project_title"
                         :short_description="pj.short_description"
                         :link="'/projects/' + pj.project_title"
                         :tags="pj.areas"
-                        :main_image="pj.main_image"/>
+                        :main_image="pj.main_image"
+                        :see_tags="true"/>
       </div>
     </div>
 

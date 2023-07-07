bb18f9a8f2a66270f9c616478212067ba3a24c57
DavideMaffi <davide1.maffi@mail.polimi.it>
2023-07-07 18:15:31 +0200

    Details in ProjectsByArea

--- components/ProjectPreview.vue
+++ components/ProjectPreview.vue
@@ -17,7 +17,7 @@ const props: Readonly<{
     <NuxtLink :to="'/projects/' + props.project_title">
       <div class="h-52 md:h-96 relative bg-center bg-cover rounded-lg hover:border-8 hover:border-slate-500 transition-all duration-200 ease-linear" :style="'background-image: url(' + props.main_image + ')'"  :id="props.project_title">
         <div v-if="props.see_tags" class="absolute top-0 right-0 flex flex-col">
-          <NuxtLink :to="'/areas/' + tag.area" v-for="tag in props.tags" >
+          <NuxtLink :to="'/areas/' + tag.area" v-for="tag in props.tags">
             <div class="dark:text-black text-white dark:hover:text-white px-2 py-1 rounded-lg"
                   :class="'bg-'+ tag.area_color +'-500 '+'hover:bg-'+tag.area_color+'-800'">
               {{ tag.area_title}}
@@ -26,7 +26,7 @@ const props: Readonly<{
         </div>
         <div class="absolute bottom-0 px-4 py-3 w-full h-12 md:h-28 overflow-hidden backdrop-blur-xl rounded-b-lg">
           <h1 class="text-black dark:text-white font-semibold text-xl md:text-3xl ml-0.5 mb-0.5 md:ml-2 md:mb-2"> {{props.project_title}} </h1>
-          <p class="text-black dark:text-white ml-2">{{props.short_description}}</p>
+          <p class="text-black dark:text-white ml-2 text-lg md:text-xl">{{props.short_description}}</p>
         </div>
       </div>
     </NuxtLink>
--- components/Projects/ProjectsByArea.vue
+++ components/Projects/ProjectsByArea.vue
@@ -1,27 +1,36 @@
 <script setup lang="ts">
+import { Project, AreaProjectPreview} from "~/model/Types";
 
 const props: Readonly<{
   projects_area?: Project[],
   area?: AreaProjectPreview,
 }> = defineProps(['projects_area', 'area']);
+
+// const details = document.getElementById(props.area!.area_title) as HTMLDetailsElement;
 </script>
 
 <template>
   <div>
-    <NuxtLink :to="'areas/' + props.area.area">
-      <div class="text-4xl text-black dark:text-white font-bold border-b-2 border-slate-500 h-fit py-4 text-center mt-2" :class="'hover:text-'+props.area?.area_color+'-500 dark:hover:text-'+props.area?.area_color+'-500'">
-        {{ props.area.area_title}}
+    <details class="text-white text-6xl w-full" open>
+      <summary class="border-b-2 border-slate-500">
+        <NuxtLink :to="'areas/' + props.area?.area" class="inline-flex">
+          <div
+              class="text-3xl sm:text-4xl text-black dark:text-white font-bold h-fit py-4 text-center mt-2"
+              :class="'hover:text-'+props.area?.area_color+'-500 dark:hover:text-'+props.area?.area_color+'-500'">
+            {{ props.area.area_title }}
+          </div>
+        </NuxtLink>
+      </summary>
+      <div class="grid xl:grid-cols-4 gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 place-content-center px-2 py-4">
+        <ProjectPreview v-for="pj in props.projects_area"
+                        :project_title="pj.project_title"
+                        :short_description="pj.short_description"
+                        :link="'/projects/' + pj.project_title"
+                        :tags="pj.areas"
+                        :see_tags="false"
+                        :main_image="pj.main_image"/>
       </div>
-    </NuxtLink>
-    <div class ="grid xl:grid-cols-4 gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 place-content-center px-2 py-4">
-      <ProjectPreview v-for="pj in props.projects_area"
-                      :project_title="pj.project_title"
-                      :short_description="pj.short_description"
-                      :link="'/projects/' + pj.project_title"
-                      :tags="pj.areas"
-                      :see_tags="false"
-                      :main_image="pj.main_image"/>
-    </div>
+    </details>
   </div>
 </template>
 
